5b5ef0360327f8537e6dc183e105dbd125cf3914
Allysonramos <canalsttyrf@gmail.com>
2026-05-24 19:24:39 -0300

    update: configuracoes de cors

--- o-pai-o/src/main/java/com/devlapa/o_pai_o/controllers/AuthenticationController.java
+++ o-pai-o/src/main/java/com/devlapa/o_pai_o/controllers/AuthenticationController.java
@@ -17,7 +17,7 @@ import java.util.Map;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
-@RequestMapping("auth")
+@RequestMapping("/auth")
 public class AuthenticationController {
 
     @Autowired
