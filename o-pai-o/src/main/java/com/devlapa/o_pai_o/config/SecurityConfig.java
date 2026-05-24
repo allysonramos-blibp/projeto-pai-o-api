14a580c62ec5e7f47f625dadb69d547a7e85b0c5
Allysonramos <canalsttyrf@gmail.com>
2026-05-24 18:58:24 -0300

    update: configuracoes

--- o-pai-o/src/main/java/com/devlapa/o_pai_o/config/SecurityConfig.java
+++ o-pai-o/src/main/java/com/devlapa/o_pai_o/config/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
 
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "https://projeto-pai-o-frontend-production.up.railway.app/"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "projeto-pai-o-front-end.vercel.app"));
 
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Auth-Token", "Accept", "X-Requested-With"));
